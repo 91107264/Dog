016e018f4df5c66ed1499795ad6c6ad28cbc97f9
91107264 <91107264@mvusd.net>
2022-12-08 12:18:02 -0800

    added labels

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,14 @@ public class Main {
         Dog dog1 = new Dog("Fang", 5, "Husky", 80);
         Dog dog2 = new Dog("Piper", 3, "Australian Shepherd", 45);
         Dog dog3 = new Dog("Rover", 1, "Golden Retriever", 20);
-        System.out.println(dog1.getBreed());
-        System.out.println(dog1.getAge());
-        System.out.println(dog2.getWeight());
+        System.out.println("Breed: " + dog1.getBreed());
+        System.out.println();
+        System.out.println("Age " + dog1.getAge());
+        System.out.println();
+        System.out.println("Weight: " + dog2.getWeight());
+        System.out.println();
         System.out.println(dog3.toString());
+        System.out.println();
         Dog.getDogs();
     }
 }
